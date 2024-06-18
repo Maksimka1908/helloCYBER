1ae51bc63cc98ea97c5d2287d9a0e800c9bca9b0
Максим <gud.maks999@gmail.com>
2024-06-18 19:14:08 +0300

    Added output in two languages

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CYBER");
         System.out.println("Привет Кибер!");
+        System.out.println("  И снова Привет");
+
     }
 }
